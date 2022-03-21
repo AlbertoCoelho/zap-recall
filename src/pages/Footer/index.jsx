bc3eb2e82c8c7c0dc55ab75c6b8650b33af27cc0
Alberto <tejotaesi@gmail.com>
2022-03-21 09:12:29 -0300

    Frase final do footer concluída

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -3,14 +3,48 @@
 // import CorrectIcon from "../../assets/correcticon.svg";
 // import { useState } from "react";
 import './style.css';
+import Sad from "../../assets/sademoticon.svg";
+import Party from "../../assets/partyemoticon.svg";
 
 const Footer = ({ answeredFlashcards,sizeDeck,footerImages,finalFooter }) => {
   if(finalFooter === "incorrect"){
-    return <h1>Footer Incorreto</h1>
+    return (
+      <div className="footer-final">
+        <div className="show-result">
+          <div className="footer-title">
+            <img src={Sad} alt="Sad emoticon" />
+            <h1>Putz...</h1>
+          </div>
+          <div className="footer-subtitle">Ainda faltam alguns...<br></br>Mas não desanime!</div>
+        </div>
+        <div className="footer-container">
+          <span>{answeredFlashcards}/{sizeDeck} CONCLUÍDOS</span>
+          <div className="icons">
+            <img src={footerImages} alt="Footer Images" /> 
+          </div>
+        </div>
+      </div>
+    );
   }
 
    else if(finalFooter === "notincorrect"){
-    return <h1>Footer Correto</h1>
+    return (
+      <div className="footer-final">
+        <div className="show-result">
+          <div className="footer-title">
+            <img src={Party} alt="Party emoticon" />
+            <h1>Parabéns!</h1>
+          </div>
+          <div className="footer-subtitle">Você não esqueceu de<br></br>nenhum flashcard!</div>
+        </div>
+        <div className="footer-container">
+          <span>{answeredFlashcards}/{sizeDeck} CONCLUÍDOS</span>
+          <div className="icons">
+            <img src={footerImages} alt="Footer Images" /> 
+          </div>
+        </div>
+      </div>
+    );
   }
 
   else{
